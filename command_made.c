d379a5c431131d2f77d1eb33cbb2927afe2350f2
esraaashraf023 <esraaashraf780567@gmail.com>
2023-08-22 17:08:37 -0700

    creat interactive mode

--- command_made.c
+++ command_made.c
@@ -7,10 +7,8 @@
  */
 void interactive_mode(void)
 {
-	ssize_t bytes;
-	size_t  input_length;
-	char *token = NULL,  *input = NULL, **argv = NULL;
-	char *command[MAXARGS];
+	ssize_t bytes;   size_t  input_length;
+	char *token = NULL,  *input = NULL, **argv = NULL, *command[MAXARGS];
 	int num = 0, j = 0;
 
 	while (1)
@@ -20,7 +18,28 @@ void interactive_mode(void)
 		bytes = getline(&input, &input_length, stdin);
 		if (bytes == EOF)
 			perror("Error input\n");
-		free(input);
 		return;
 	}
-}
+	if (token != NULL && token[0] == '\n' && bytes == 1)
+                return;
+        token = strtok(input, " ");
+        while (token != NULL)
+                command[num++] = token;
+        token = strtok(NULL, " \n");
+        command[num] = NULL;
+        argv = (char **)malloc((num + 1) * sizeof(char *));
+        if (argv == NULL)
+        {
+                perror("failed\n");
+                return;
+        }
+        for (; j < num; j++)
+                argv[j] = command[j];
+        argv[num] = argv[j] = command[j];
+        argv[num] = NULL;
+        execve_commandline(argv);
+        {
+		free(tonken);
+                free(argv);
+                free(input);
+        }
