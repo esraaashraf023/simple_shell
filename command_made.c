bc83a4c1a0afb237aa2a15ce13260b07e0de9eca
esraaashraf023 <esraaashraf780567@gmail.com>
2023-08-22 17:16:20 -0700

    creat interative mode

--- command_made.c
+++ command_made.c
@@ -7,7 +7,8 @@
  */
 void interactive_mode(void)
 {
-	ssize_t bytes;   size_t  input_length;
+	ssize_t bytes;
+	size_t input_length;
 	char *token = NULL,  *input = NULL, **argv = NULL, *command[MAXARGS];
 	int num = 0, j = 0;
 
@@ -21,25 +22,26 @@ void interactive_mode(void)
 		return;
 	}
 	if (token != NULL && token[0] == '\n' && bytes == 1)
-                return;
-        token = strtok(input, " ");
-        while (token != NULL)
-                command[num++] = token;
-        token = strtok(NULL, " \n");
-        command[num] = NULL;
-        argv = (char **)malloc((num + 1) * sizeof(char *));
-        if (argv == NULL)
-        {
-                perror("failed\n");
-                return;
-        }
-        for (; j < num; j++)
-                argv[j] = command[j];
-        argv[num] = argv[j] = command[j];
-        argv[num] = NULL;
-        execve_commandline(argv);
-        {
-		free(tonken);
-                free(argv);
-                free(input);
-        }
+		return;
+	token = strtok(input, " ");
+	while (token != NULL)
+		command[num++] = token;
+	token = strtok(NULL, " \n");
+	command[num] = NULL;
+	argv = (char **)malloc((num + 1) * sizeof(char *));
+	if (argv == NULL)
+	{
+		perror("failed\n");
+		return;
+	}
+	for (; j < num; j++)
+		argv[j] = command[j];
+	argv[num] = argv[j] = command[j];
+	argv[num] = NULL;
+	execve_commandline(argv);
+	{
+		free(token);
+		free(argv);
+		free(input);
+	}
+}
